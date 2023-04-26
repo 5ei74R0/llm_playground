d960bcf3b943b4dbd82befffe3211d5827c3b623
5ei74R0 <otoxukk.y@gmail.com>
2023-04-26 16:21:36 +0900

    [add] setup python

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+Entrypoint of `llm_playground`.
+"""
+import fire
+
+
+def main(
+    dataset: str,
+    model: str,
+):
+    print(f"dataset: {dataset}")
+    print(f"model: {model}")
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
